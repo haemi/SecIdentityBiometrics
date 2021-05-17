da301269a1c81e4037988f3bd153ed9e5c7d6c56
Stefan Walkner <stefan@arkulpa.at>
2021-05-17 13:12:16 +0200

    Initial Commit

---
+++ KCPG/KCPGApp.swift
@@ -0,0 +1,17 @@
+//
+//  KCPGApp.swift
+//  KCPG
+//
+//  Created by Stefan Walkner on 17.05.21.
+//
+
+import SwiftUI
+
+@main
+struct KCPGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
